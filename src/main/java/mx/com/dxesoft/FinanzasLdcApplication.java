8642bb33eb736c460563f0fc1cadbdbda399591e
Ernesto Mendez <nernesto@gmail.com>
2017-08-02 00:54:33 -0700

    First commit

---
+++ src/main/java/mx/com/dxesoft/FinanzasLdcApplication.java
@@ -0,0 +1,12 @@
+package mx.com.dxesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzasLdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzasLdcApplication.class, args);
+	}
+}
---
+++ src/test/java/mx/com/dxesoft/FinanzasLdcApplicationTests.java
@@ -0,0 +1,16 @@
+package mx.com.dxesoft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinanzasLdcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
